9cd6289bf13ebda899d8d15961216fe5d49f8421
Bakerman <74716816+DanilMetelkin@users.noreply.github.com>
2022-01-21 02:49:01 +0600

    adding a file of site

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.navigation__list');
+const body = document.querySelector('body');
+
+
+burger.addEventListener('click', () => {
+menu.classList.toggle('active');
+burger.classList.toggle('active');
+body.classList.toggle('lock');
+})
